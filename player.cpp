38e6c94277cca5c296da15de25b405efe21cbc13
Alexander Popov <alexanderpopov161@gmail.com>
2018-03-04 21:59:35 -0800

    Yay a working heuristic function (based on stability & piece difference)

--- player.cpp
+++ player.cpp
@@ -17,10 +17,10 @@ Player::Player(Side side) {
      board = new Board();
      playerSide = side;
      if(playerSide == WHITE){
-         opponentSize = BLACK;
+         opponentSide = BLACK;
      }
      else if(playerSide == BLACK){
-         opponentSize = WHITE;
+         opponentSide = WHITE;
      }
 }
 
@@ -29,6 +29,147 @@ Player::Player(Side side) {
  */
 Player::~Player() {
     delete board;
+}
+
+int Player::getScore(Move *move){
+    int score = 0;
+    Board* testBoard = board->copy();
+    testBoard->doMove(move, playerSide);
+
+    int playerCount = testBoard->count(playerSide);
+    int opponentCount = testBoard->count(opponentSide);
+
+    //if(playerCount + opponentCount > 30){
+    score += (playerCount - opponentCount);
+    //}
+    //else{
+    //    score -= opponentCount;
+    //}
+
+    //score += this->checkStables(testBoard);
+    score += 2*this->checkStables(testBoard);
+    score += 3*this->checkCorners(testBoard);
+
+
+    return score;
+
+}
+
+int Player::checkCorners(Board *board){
+    int score = 0;
+
+    if(board->get(playerSide,0,0)){
+        score += 1;
+    }
+    if(board->get(playerSide,0,7)){
+        score += 1;
+    }
+    if(board->get(playerSide,7,0)){
+        score += 1;
+    }
+    if(board->get(playerSide,7,7)){
+        score += 1;
+    }
+
+    return score;
+}
+
+int Player::checkStables(Board *board){
+    int count = 0;
+    bool stable[8][8];
+    bool checked[8][8];
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            checked[i][j] = false;
+        }
+    }
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(this->stableCell(i, j, board, stable, checked)){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+bool Player::stableCell(int x, int y, Board *board, bool stable[][8], bool checked[][8]){
+    if(checked[x][y]){
+        return stable[x][y];
+    }
+    checked[x][y] = true;
+    if(!board->get(playerSide,x,y)){
+        checked[x][y] = true;
+        stable[x][y] = false;
+        return false;
+    }
+
+    //check row stability
+    bool lStable = true;
+    bool rStable = true;
+    for(int i = x+1; i < 8; i++){
+        if(!stableCell(i, y, board, stable, checked)) rStable = false;
+    }
+    for(int i = x-1; i >= 0; i--){
+        if(!stableCell(i, y, board, stable, checked)) lStable = false;
+    }
+
+    //check column stability
+    bool uStable = true;
+    bool dStable = true;
+    for(int i = y+1; i < 8; i++){
+        if(!stableCell(x, i, board, stable, checked)) uStable = false;
+    }
+    for(int i = y-1; i >= 0; i--){
+        if(!stableCell(x, i, board, stable, checked)) dStable = false;
+    }
+
+    //check right-up/left-down diagonal stability
+    bool urStable = true;
+    bool dlStable = true;
+
+    int tx = x+1;
+    int ty = y+1;
+    while(tx < 8 && ty < 8){
+        if(!stableCell(tx, ty, board, stable, checked)) urStable = false;
+        tx++;
+        ty++;
+    }
+
+    tx = x-1;
+    ty = y-1;
+    while(tx > 0 && ty > 0){
+        if(!stableCell(tx, ty, board, stable, checked)) dlStable = false;
+        tx--;
+        ty--;
+    }
+
+    //check right-down/left-up diagonal stability
+    bool ulStable = true;
+    bool drStable = true;
+
+    tx = x+1;
+    ty = y-1;
+    while(tx < 8 && ty > 0){
+        if(!stableCell(tx, ty, board, stable, checked)) drStable = false;
+        tx++;
+        ty--;
+    }
+
+    tx = x-1;
+    ty = y+1;
+    while(tx > 0 && ty < 8){
+        if(!stableCell(tx, ty, board, stable, checked)) ulStable = false;
+        tx--;
+        ty++;
+    }
+
+
+    checked[x][y] = true;
+    stable[x][y] = (lStable || rStable) && (uStable || dStable) &&
+            (dlStable || urStable) && (ulStable || drStable);
+    //std::cerr << stable[x][y] << std::endl;
+    return stable[x][y];
 }
 
 /*
@@ -50,18 +191,26 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * process the opponent's opponents move before calculating your own move
      */
 
-     board->doMove(opponentsMove, opponentSize);
+     board->doMove(opponentsMove, opponentSide);
 
-     //Move moves[10];
+     Move *best = nullptr;
+     int bestScore = -64;
      for (int i = 0; i < 8; i++) {
          for (int j = 0; j < 8; j++) {
-             Move move(i, j);
-             if (board->checkMove(&move, playerSide)){
-                 board->doMove(&move, playerSide);
-                 return new Move(i,j);
+             Move *move = new Move(i, j);
+             if (board->checkMove(move, playerSide)){
+                 int score = this->getScore(move);
+                 if(score > bestScore){
+                     bestScore = score;
+                     best = move;
+                 }
+                 else{
+                     delete move;
+                 }
              }
          }
      }
-     return nullptr;
+     board->doMove(best, playerSide);
+     return best;
 
 }
--- player.hpp
+++ player.hpp
@@ -13,12 +13,16 @@ public:
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
+    int getScore(Move *move);
+    int checkCorners(Board *board);
+    int checkStables(Board *board);
+    bool stableCell(int x, int y, Board *board, bool stable[][8], bool checked[][8]);
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
     Board *board;
     Side playerSide;
-    Side opponentSize;
+    Side opponentSide;
 };
 
 #endif
